4aa873d729d607d59cc8db30f0cefb5b6cc4255d
Ishika Mahto <ishikamahto9@gmail.com>
2025-08-12 22:50:27 +0530

    Add files via upload

---
+++ Assignment_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float num1, num2, diff;
+    printf("Enter two floating numbers : \n");
+    scanf("%f %f", &num1, &num2);
+    // diff = num1 - num2;
+    printf("Difference = %f\n", num1-num2);
+    return 0;
+}
