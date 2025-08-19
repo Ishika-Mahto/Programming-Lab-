6b2033db15065164507668b320d3370537df11d5
Ishika Mahto <ishikamahto9@gmail.com>
2025-08-19 16:20:01 +0530

    Assignment_8.c

--- Assignment_8.c
+++ Assignment_8.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+
 int main()
 {
-	float value_f, value_c, ;
-	printf("Enter the value in fahrenheit : \n");
-	scanf("%f",&value_f);
-	printf("Enter the value in celcius : \n");
-	scanf("&f",&value_c);
-	c = 5/9*(value_f-32);
-	f = (9/5*value_c)+32;
-	printf("Fahrenheit to celcius is : %f",c);
-	printf("Celcius to fahrenheit is : %f",f);
-	return 0;
+    float value_f, value_c, c, f;
+
+    printf("Enter the value in Fahrenheit: \n");
+    scanf("%f", &value_f);
+
+    printf("Enter the value in Celsius: \n");
+    scanf("%f", &value_c);
+
+    c = (value_f - 32) * (5.0 / 9.0);
+    f = (value_c * (9.0 / 5.0)) + 32;
+
+    printf("Fahrenheit to Celsius is: %.2f\n", c);
+    printf("Celsius to Fahrenheit is: %.2f\n", f);
+
+    return 0;
 }
