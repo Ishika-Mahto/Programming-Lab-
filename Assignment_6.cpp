8ed4b5f2dd8c49db984dc2c4a2156970d4d028d8
Ishika Mahto <ishikamahto9@gmail.com>
2025-08-21 21:17:06 +0530

    Assignment_6.c

--- Assignment_6.cpp
+++ Assignment_6.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	float principle, rate, time, simple_interest;
-	printf("Enter principle, rate and time : \n");
-	scanf("%f",&principle);
+	float principal, rate, time, simple_interest;
+	printf("Enter principal, rate and time : \n");
+	scanf("%f",&principal);
 	scanf("%f",&rate);
 	scanf("%f",&time);
-	simple_interest = (principle*time*rate)/100;
+	simple_interest = (principal*time*rate)/100;
 	printf("The simple interest is : %f",simple_interest);
 	return 0;
 }
